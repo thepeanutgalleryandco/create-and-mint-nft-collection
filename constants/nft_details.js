48912e5bc31664937d11b2639544f479aa2147a8
rohan <rohan.dejongh@vodafone.com>
2022-01-24 11:08:07 +0200

    Security enhancements
    Added change for with regards to variable name change and implemented the logic in src/main.js - https://github.com/thepeanutgalleryandco/create-and-mint-nft-collection/issues/6
    
    Implemented security fixes:
    Updated node-fetch from 2.6.6 to 2.6.7
    Updated ansi-regex from 3.0.0 to 5.0.1
    
    Updated README.

--- constants/nft_details.js
+++ constants/nft_details.js
@@ -3,7 +3,7 @@ const NFT_DETAILS = {
   namePrefix: "YOUR_NAME_PREFIX_HERE", // Set the name prefix that will be added for each of your NFTs. Ex. Steaks #1, Steaks #2
   imageFilesBase: "ipfs://ADD_IPFS_IMAGE_CID_HERE", // Set the pinned IPFS CID when making use of a service like Pinata. Ex. QmP12prm2rp1omr1Ap2orm1oprm12FQWFQOFOGdnasnsda
   metaDataJSONFilesBase: "ipfs://ADD_IPFS_META_DATA_CID_HERE", // Set the pinned IPFS CID when making use of a service like Pinata. Ex. QmP12prm2rp1omr1Ap2orm1oprm12FQWFQOFOGdnasndca
-  blankFilenameInLayers: true, // This value is a boolean with a value of false or true. If true, then any layer items where the image name is blank, then it will skip that layer when adding metadata information. When set to false, then the information will be added to the metadata.
+  ignoreExactBlankName: true, // This value is a boolean with a value of false or true. If true, then any layer items where the image name is blank, then it will skip that layer when adding metadata information. When set to false, then the information will be added to the metadata.
   genericTitle: "GENERIC_NAMEPREFIX_HERE", // Set the generic name prefix that you want the NFTs to have. Only change if you are planning on using NFT reveal. E.x Unknown
   genericDescription: "GENERIC_DESCRIPTION_HERE", // Set the generic description that you want the NFTs to have. Only change if you are planning on using NFT reveal. E.x Unknown
   genericURL: "GENERIC_URL_HERE", // Set the generic image URL that you want the NFTs to have. Only change if you are planning on using NFT reveal. E.x https://ipfs.io/ipfs/QmX6GnNspTEUUCzv1woUGT83jXC3fYZ4SuMxeoSGZQF9sA/logo.gif
